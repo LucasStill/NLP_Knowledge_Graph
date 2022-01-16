1e11f56b3645681728e423aa8c09460f71258095
GitHubByJelle <71011269+GitHubByJelle@users.noreply.github.com>
2022-01-16 12:25:52 +0100

    Updated github

--- kindred.py
+++
@@ -1,8 +0,0 @@
-# https://docs.allennlp.org/models/main/
-from allennlp_models import pretrained
-
-model = pretrained.load_predictor("coref-spanbert")
-
-test = model.predict_instance("This is a test. Do you think it will work?")
-
-print(test)
